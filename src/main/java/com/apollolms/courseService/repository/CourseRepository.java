b62fb31280ecbc82fe4604f8714be6697dfacdfc
Apollos <severeapollos@gmail.com>
2024-01-05 14:11:46 -0500

    update aws configuration

--- src/main/java/com/apollolms/courseService/repository/CourseRepository.java
+++ src/main/java/com/apollolms/courseService/repository/CourseRepository.java
@@ -1,7 +1,7 @@
 package com.apollolms.courseService.repository;
 
 import com.apollolms.courseService.entity.Course;
-import org.springframework.data.mongodb.repository.Query;;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
